e12cb62e77f4dbef1d308e33ba5105301be6fbc6
IsaacXH <91279373+IsaacXH@users.noreply.github.com>
2022-10-17 15:25:02 -0700

    few changes to reticles

--- IsaacVRRoom/Assets/Rotation.cs
+++ IsaacVRRoom/Assets/Rotation.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
+    public Vector3 rotation;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Rotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(rotation * speed * Time.deltaTime);
     }
 }

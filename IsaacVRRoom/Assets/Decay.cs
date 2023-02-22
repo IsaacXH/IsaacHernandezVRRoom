04a1852d501795e6bad7309b8268b76473020050
IsaacXH <739319@student.dusd.net>
2023-02-22 09:58:24 -0800

    added new scene to build and added UI for teleportation

--- IsaacVRRoom/Assets/Decay.cs
+++ IsaacVRRoom/Assets/Decay.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveFoward : MonoBehaviour
+public class Decay : MonoBehaviour
 {
-    public float speed = 40.0f;
     // Start is called before the first frame update
     void Start()
     {
-      
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-       transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        if(transform.position.magnitude > 50.0f)
+    {
+        Destroy(gameObject);
+    }
     }
 }
